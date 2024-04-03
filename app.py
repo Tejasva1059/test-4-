502c60c96464d0c757dcc2025d2f2efc3cfb35bc
UMANG GOSWAMI <go.umang108@gmail.com>
2024-04-03 10:23:02 +0530

    Update app.py
    Signed-off-by: UMANG GOSWAMI <go.umang108@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+# FullStack Development done by UMANG GOSWAMI (22100BTCSAII11062) github.com/goswamiumang108
+
+
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
